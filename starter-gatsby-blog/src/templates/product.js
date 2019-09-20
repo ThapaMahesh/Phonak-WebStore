180e8a847977e5a2af96cf8dbe72c736d0d996fe
Mahesh Bahadur Thapa <mahesh.thapa@makingwaves.com>
2019-09-20 14:59:52 +0200

    variants and update sku on variant changes

--- starter-gatsby-blog/src/templates/product.js
+++ starter-gatsby-blog/src/templates/product.js
@@ -7,7 +7,32 @@ import * as CLayer from 'commercelayer-react'
 import heroStyles from '../components/hero.module.css'
 import styles from '../components/article-preview.module.css'
 
+import VariantTypes from '../components/variant-types'
+
 class ProductTemplate extends React.Component {
+
+  
+  constructor(props){
+    super(props)
+
+
+    this.product = get(props, 'data.contentfulProductsList')
+    this.siteTitle = get(props, 'data.site.siteMetadata.title')
+
+    this.types = this.product.variant.map(eachVariant => eachVariant.type != null ? eachVariant.type.name : null).filter(this.distinct).filter(Boolean)
+
+    this.sizes = this.product.variant.map(eachVariant => eachVariant.size != null ? eachVariant.size.size : null).filter(this.distinct).filter(Boolean)
+
+    this.color = this.product.variant.map(eachVariant => eachVariant.productColour != null ? eachVariant.productColour.colour : null).filter(this.distinct).filter(Boolean)
+
+    this.variants = this.getVariant(this.product)
+
+    this.currentVariant = this.getCode(this.variants, this.types[0], this.sizes[0], this.color[0])
+
+    this.state = {
+      code: this.currentVariant.code
+    }
+  }
 
   getVariant(product){
     let variantArray = []
@@ -15,9 +40,9 @@ class ProductTemplate extends React.Component {
     for (var i = 0; i < product.variant.length; i++) {
       variantArray.push({
         code: product.variant[i].code, 
-        type: product.variant[i].type != null ? product.variant[i].type.name : "",
-        size: product.variant[i].size != null ? product.variant[i].size.size : "",
-        color: product.variant[i].productColour != null ? product.variant[i].productColour.colour : ""
+        type: product.variant[i].type != null ? product.variant[i].type.name : null,
+        size: product.variant[i].size != null ? product.variant[i].size.size : null,
+        color: product.variant[i].productColour != null ? product.variant[i].productColour.colour : null
       }) 
     }
 
@@ -28,80 +53,67 @@ class ProductTemplate extends React.Component {
     return self.indexOf(value) === index
   }
 
-  render() {
-    const product = get(this.props, 'data.contentfulProductsList')
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+  getCode(allVariants, type, size, color){
+    type = (type == null || type == undefined || type == "") ? null : type
+    color = (color == null || color == undefined || color == "") ? null : color
+    size = (size == null || size == undefined || size == "") ? null : size
 
-    let types = product.variant.map(eachVariant => eachVariant.type != null ? eachVariant.type.name : "").filter(this.distinct)
-
-    let sizes = product.variant.map(eachVariant => eachVariant.size != null ? eachVariant.size.size : "").filter(this.distinct)
-
-    let color = product.variant.map(eachVariant => eachVariant.productColour != null ? eachVariant.productColour.colour : "").filter(this.distinct)
-
-    let variants = this.getVariant(product)
-
-    console.log(color)
-    window.onload = function(){
-      if(sizes.length === 1 && sizes[0] === ""){
-        document.getElementById("select-size").style.display = "none"
-      }
-
-      if(types.length === 1 && types[0] === ""){
-        document.getElementById("select-type").style.display = "none"
-      }
-
-      if(color.length === 1 && color[0] === ""){
-        document.getElementById("select-color").style.display = "none"
+    let result = {code: null, type: type, color: color, size: size}
+    for (var i = 0; i < allVariants.length; i++) {
+      if(allVariants[i].type === type && allVariants[i].size === size && allVariants[i].color === color){
+        result = {
+          code: allVariants[i].code,
+          type: type,
+          color: color,
+          size: size
+        }
       }
     }
+    console.log(result)
+    return result
+  }
+  
+ 
+
+  onSelectChange = (value, type) => {
+    // update currentvariant type value to the changed one before getting code
+    this.currentVariant[type] = value
+
+    this.currentVariant = this.getCode(this.variants, this.currentVariant.type, this.currentVariant.size, this.currentVariant.color)
+    
+    console.log(this.currentVariant)
+    this.setState({code: this.currentVariant.code})
+  }
+
+  render() {
+    
 
     return (
       <div style={{ background: '#fff' }}>
-        <Helmet title={`${product.name} | ${siteTitle}`} />
+        <Helmet title={`${this.product.name} | ${this.siteTitle}`} />
 
         <div className={styles.flexdiv}>
             <div className={styles.span6}>
-              <img src={product.picture} />
+              <img src={this.product.picture} />
             </div>
           <div className={styles.span4}>
               <div className={styles.sProdContainer}>
-                <h3>{product.name}</h3>
-                <CLayer.Price skuCode="Hook HE 7"/>
+                <h3>{this.product.name}</h3>
+                <CLayer.Price skuCode={this.state.code}/>
+                <CLayer.AvailabilityMessageContainer id={`availability-message-${this.state.code}`} />
               </div>
-                  <div id="select-color" className={styles.variationDiv}>
-                        <p>Velg farge:</p>
-                        <select>
-                            { color.map(function(eachColor, index){ 
-                                return <option key={index}>{eachColor}</option>
-                              })
-                            }
-                        </select>
-                  </div>
-                  <div id="select-type" className={styles.variationDiv}>
-                      <p>Velg type:</p>
-                      <select>
-                          { types.map(function(eachType, index){ 
-                                return <option key={index}>{eachType}</option>
-                              })
-                            }
-                      </select>
-                  </div>
-                  <div id="select-size" className={styles.variationDiv}>
-                      <p>Velg størrelse:</p>
-                      <select>
-                          { sizes.map(function(eachSize, index){ 
-                                return <option key={index}>{eachSize}</option>
-                              })
-                            }
-                      </select>
-                  </div>
+              
+              <VariantTypes variant={this.color} title={'Velg Farge:'} type={'color'} onSelectChange={this.onSelectChange}/>
+              <VariantTypes variant={this.types} title={'Velg Types: '} type={'type'} onSelectChange={this.onSelectChange}/>
+              <VariantTypes variant={this.sizes} title={'Velg størelse: '} type={'size'} onSelectChange={this.onSelectChange}/>
+              
             <div className={styles.checkoutDiv}>
               <button type="button" className={styles.button}>Add to Cart</button> 
-              <p>{product.price} Kr</p> 
+              <p>{this.product.price} Kr</p> 
               <input className={styles.quantity}
                 id="add-to-bag-quantity"
                 type="number"
-                value="1"
+                defaultValue="1"
               />
             </div>
           </div>
